fc20370081f0ff23fe70480e92267e14877d6d05
Nick Nobile <nickcnobile@gmail.com>
2020-09-26 21:47:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_array = self.split(/\.|\?|\!/)
+    split_array.reject!{|x| x.empty?}
+    split_array.count
   end
 end
